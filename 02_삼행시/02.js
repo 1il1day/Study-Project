11a8e299f236debaa1af228c0977c0b39d3d6fe1
1il1day <100670176+1il1day@users.noreply.github.com>
2022-03-13 14:29:49 +0900

    Add files via upload

---
+++ 02_삼행시/02.js
@@ -0,0 +1,14 @@
+function mo(){
+    const p1 = document.querySelector('p:nth-child(1)');
+    p1.innerHTML = "모처럼이니";
+}
+
+function gak(){
+    const p2 = document.querySelector('p:nth-child(2)');
+    p2.innerHTML = "각잡고 열심히 하면";
+}
+
+function ko(){
+    const p3 = document.querySelector('p:nth-child(3)');
+    p3.innerHTML = "코린이 탈출이다";
+}
